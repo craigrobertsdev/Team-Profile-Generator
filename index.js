e564f74cb3a121a69a95f6324df87835e52bfc9b
Craig Roberts <craig.roberts11@outlook.com>
2023-02-18 05:34:03 +1030

    Question loop working and html generating correctly

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const inquirer = require("inquirer");
-const buildTeamRoster = require("./lib/html-builder");
+const filebuilder = require("./lib/html-builder");
 const Manager = require("./src/Manager");
 const Engineer = require("./src/Engineer");
 const Intern = require("./src/Intern");
@@ -11,47 +11,47 @@ const teamMembers = [];
 const addNewMember = {
   type: "list",
   message: "Add another member to your team?",
-  options: ["Engineer", "Intern", new separator(), "Finished adding team members"],
+  choices: ["Engineer", "Intern", new inquirer.Separator(), "Finished adding team members"],
   name: "newMember",
 };
 
-const addEmployee = [
-  {
-    type: "input",
-    message: `Enter the ${employeeType}'s name:`,
-    name: "name",
-  },
-  {
-    type: "input",
-    message: "What is their ID number?",
-    name: "id",
-  },
-  {
-    type: "input",
-    message: "What is their email address?",
-    name: "email",
-  },
-];
+// const addEmployee = [
+//   {
+//     type: "input",
+//     message: `Enter the ${employeeType}'s name:`,
+//     name: "name",
+//   },
+//   {
+//     type: "input",
+//     message: "What is their ID number?",
+//     name: "id",
+//   },
+//   {
+//     type: "input",
+//     message: "What is their email address?",
+//     name: "email",
+//   },
+// ];
 
 const addManager = {
   type: "input",
   message: "What is their office number?",
-  name: "extra",
+  name: "office",
 };
 
 const addEngineer = {
   type: "input",
   message: "What is their GitHub profile?",
-  name: "extra",
+  name: "github",
 };
 
 const addIntern = {
   type: "input",
   message: "What school do they go to?",
-  name: "extra",
+  name: "school",
 };
 
-function askQuestions(employeeType) {
+async function askQuestions(employeeType) {
   let extraQuestion;
   if (employeeType === "Manager") {
     extraQuestion = addManager;
@@ -61,33 +61,68 @@ function askQuestions(employeeType) {
     extraQuestion = addIntern;
   }
 
-  inquirer.prompt([...addEmployee, extraQuestion, addNewMember]).then((response) => {
-    const { name, id, email, extra, newMember } = response;
+  await inquirer
+    .prompt([
+      {
+        type: "input",
+        message: `Enter the ${employeeType}'s name:`,
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is their ID number?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is their email address?",
+        name: "email",
+      },
+      extraQuestion,
+      addNewMember,
+    ])
+    .then(async (response) => {
+      if (employeeType === "Manager") {
+        const { name, id, email, office } = response;
+        teamMembers.push(new Manager(name, id, email, office));
+      } else if (employeeType === "Engineer") {
+        const { name, id, email, github } = response;
+        teamMembers.push(new Engineer(name, id, email, github));
+      } else {
+        const { name, id, email, school } = response;
+        teamMembers.push(new Intern(name, id, email, school));
+      }
 
-    if (employeeType === "Manager") {
-      teamMembers.push(new Manager(name, id, email, extra));
-    } else if (employeeType === "Engineer") {
-      teamMembers.push(new Engineer(name, id, email, extra));
-    } else {
-      teamMembers.push(new Intern(name, id, email, extra));
-    }
+      employeeType = response.newMember;
+      console.log("Passing newMember as [");
+      for (let member of teamMembers) {
+        console.log(member);
+      }
+      console.log("]");
 
-    if (newMember.startsWith("Finished")) {
-      return;
-    }
-
-    askQuestions(newMember);
-  });
+      // break out of loop if user is done adding team members;
+      if (response.newMember.startsWith("Finished")) {
+        return;
+      }
+      await askQuestions(response.newMember);
+    });
 }
 
 function saveTeamHtml(html) {
   fs.writeFileSync("./dist/roster.html", html);
 }
 
-function init() {
-  askQuestions("Manager");
-  const teamHtml = buildTeamRoster(teamMembers);
+function saveCss(css) {
+  fs.writeFileSync("./dist/roster.css", css);
+}
+
+async function init() {
+  await askQuestions("Manager");
+  console.log("passing team members" + teamMembers);
+  const teamHtml = filebuilder.buildTeamRoster(teamMembers);
   saveTeamHtml(teamHtml);
+  const css = filebuilder.buildCss();
+  saveCss(css);
 }
 
 init();
